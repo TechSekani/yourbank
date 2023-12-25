3ab2b77678de8b607ae52bcfc7c56e4e06d76a05
TechSekani <116392903+TechSekani@users.noreply.github.com>
2023-12-25 23:58:14 +0100

    commit

--- src/components/PagesHero.jsx
+++ src/components/PagesHero.jsx
@@ -3,8 +3,8 @@ import { CareerImg } from "../assets/images";
 
 const PagesHero = () => {
   return (
-    <section className="relative md:w-full h-[576px] md:h-[550px] mx-4 md:mx-0 mt-[110px] mb-24 rounded-2xl md:p-[50px]  bg-[#1c1c1c]">
-      <div className=" text-center md:text-start absolute top-[210px] md:top-[50px] right-[16px] md:left-[50px] z-20 w-[91%] md:w-[48%] bg-[#1a1a1a] p-6 md:p-10 rounded-2xl md:rounded-none md:rounded-br-3xl">
+    <section className="relative md:w-full h-full md:h-[550px] mx-4 md:mx-0 mt-[110px] mb-24 rounded-2xl py-[20px] md:p-[50px]  bg-[#1c1c1c]">
+      <div className=" text-center md:text-start md:absolute top-[210px] md:top-[50px] right-[16px] md:left-[50px] z-20 w-[91%] md:w-[48%] bg-[#1a1a1a] p-6 md:p-10 mx-auto md:mx-0 rounded-2xl md:rounded-none md:rounded-br-3xl  mt-[70%] md:mt-0">
         <h3 className=" text-2xl md:text-4xl font-medium">
           Welcome to <span>YourBank</span> Careers!
         </h3>
